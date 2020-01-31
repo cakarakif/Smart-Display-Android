6cb1850ae572c3bb02d5be697bfd51e3a10b9632
cakarakif <cakarakif@users.noreply.github.com>
2020-01-31 18:38:12 +0300

    design v4

---
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/LoginPage/ResetPassword/resetpass.java
@@ -0,0 +1,16 @@
+package com.example.smartdisplay.LoginPage.ResetPassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartdisplay.R;
+
+public class resetpass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpass);
+    }
+}
