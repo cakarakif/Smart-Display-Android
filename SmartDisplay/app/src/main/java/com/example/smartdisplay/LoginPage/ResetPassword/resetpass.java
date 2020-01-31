772e34922935017224ff4f3c7eaf9112e533f1a5
cakarakif <cakarakif@users.noreply.github.com>
2020-01-31 18:57:16 +0300

    login rotates

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/LoginPage/ResetPassword/resetpass.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/LoginPage/ResetPassword/resetpass.java
@@ -2,15 +2,41 @@ package com.example.smartdisplay.LoginPage.ResetPassword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.smartdisplay.R;
 
 public class resetpass extends AppCompatActivity {
 
+    private TextView signin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpass);
+        //Tam ekran kullanımı için
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        define();
+    }
+
+    private void define() {
+        signin = findViewById(R.id.signin);
+
+        rotate();
+    }
+
+    private void rotate() {
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intnt = new Intent(getApplicationContext(), com.example.smartdisplay.LoginPage.SignIn.signin.class);
+                startActivity(intnt);
+            }
+        });
     }
 }
--- SmartDisplay/app/src/main/java/com/example/smartdisplay/LoginPage/SignIn/signin.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/LoginPage/SignIn/signin.java
@@ -2,12 +2,20 @@ package com.example.smartdisplay.LoginPage.SignIn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.example.smartdisplay.LoginPage.ResetPassword.resetpass;
+import com.example.smartdisplay.LoginPage.SignUp.signup;
+import com.example.smartdisplay.MainActivity;
 import com.example.smartdisplay.R;
 
 public class signin extends AppCompatActivity {
+
+    private TextView forgotPass,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,34 @@ public class signin extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
         //Tam ekran kullanımı için
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        define();
+    }
+
+    private void define(){
+        forgotPass=findViewById(R.id.forgotPass);
+        signup=findViewById(R.id.signup);
+
+        rotate();
+    }
+
+    private void rotate(){
+        //şifre yenilemeye yönlendirildi
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intnt=new Intent(getApplicationContext(), resetpass.class);
+                startActivity(intnt);
+            }
+        });
+
+        //kayıta yönlendirildi.
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intnt=new Intent(getApplicationContext(), com.example.smartdisplay.LoginPage.SignUp.signup.class);
+                startActivity(intnt);
+            }
+        });
     }
 }
