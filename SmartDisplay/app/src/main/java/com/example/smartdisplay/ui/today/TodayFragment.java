a4ccc3b75ef7346a8a0ed360f8d2eb2a5e0499f5
cakarakif <cakarakif@users.noreply.github.com>
2020-02-29 16:33:58 +0300

    Connected today list structures

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/today/TodayFragment.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/today/TodayFragment.java
@@ -1,47 +1,85 @@
 package com.example.smartdisplay.ui.today;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.smartdisplay.Adapter.TaskListAdapter;
+import com.example.smartdisplay.DatabaseHelperClasses.UserTask;
 import com.example.smartdisplay.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class TodayFragment extends Fragment {
     private View root;
 
+    private ListView taskListView;
+    private List<UserTask> taskList, filteredList;
+    private TaskListAdapter listAdapter;
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+
+    private ProgressDialog loading;
+
     private LinearLayout one,two,three,four,five,six,seven;
     private TextView oneName,oneDate, twoName,twoDate, threeName,threeDate, fourName,fourDate, fiveName,fiveDate, sixName,sixDate,sevenName,sevenDate;
+    private Calendar cal;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_today, container, false);
 
+        cal = Calendar.getInstance();//ilk başta çekilir sadece
 
         define();
         routing();
 
-        int numberMov=2;
-        dateCalendar(numberMov);
+        readUserTasks();
+        //temel calendar yapısı oluşturulup gerekli yerlerde çağrıldı
+        dateCalendar(0);
+
 
 
         return root;
     }
 
     private void define(){
+        taskListView =root.findViewById(R.id.taskListView);
+
+        //kullancıya özel database bilgi ekleme/alma için eklendi
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference(user.getUid() + "/Tasks");
+
         one = root.findViewById(R.id.one);
         two = root.findViewById(R.id.two);
         three = root.findViewById(R.id.three);
@@ -67,7 +105,54 @@ public class TodayFragment extends Fragment {
     }
 
     private void routing(){
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(-3);
+            }
+        });
 
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(-2);
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(-1);
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(0);
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(1);
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(2);
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateCalendar(3);
+            }
+        });
     }
 
     private void dateCalendar(int numberMov){
@@ -75,37 +160,135 @@ public class TodayFragment extends Fragment {
         //number kaç ileri-geri gittiği alınarak sabit mantık işletildi.
 
         Locale.setDefault(Locale.ENGLISH);
-        Calendar cal = Calendar.getInstance();
 
         cal.add(cal.DATE, numberMov);
         fourDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         fourName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
         //ileri üç gün
-        cal.add(Calendar.DATE, 1);
+        cal.add(cal.DATE, 1);
         fiveDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         fiveName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
-        cal.add(Calendar.DATE, 1);
+        cal.add(cal.DATE, 1);
         sixDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         sixName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
-        cal.add(Calendar.DATE, 1);
+        cal.add(cal.DATE, 1);
         sevenDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         sevenName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
         //geri üç gün
-        cal.add(Calendar.DATE, -4);
+        cal.add(cal.DATE, -4);
         threeDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         threeName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
-        cal.add(Calendar.DATE, -1);
+        cal.add(cal.DATE, -1);
         twoDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         twoName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
 
-        cal.add(Calendar.DATE, -1);
+        cal.add(cal.DATE, -1);
         oneDate.setText(cal.get(cal.DAY_OF_MONTH)+"");
         oneName.setText((cal.getDisplayName(cal.DAY_OF_WEEK,cal.SHORT, Locale.getDefault())).substring(0,2).toUpperCase());
+
+        //default almak için dördüncüye çekildi
+        cal.add(cal.DATE, 3);
+
+        //her seferinde liste üzerinden yeniden filtreleme yapıldı
+        filterList();
+    }
+
+    private void readUserTasks() {
+        loading = ProgressDialog.show(getContext(), "Please wait...", "Retrieving data ...", true);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Log.i("tasks", dataSnapshot.getValue() + "");
+
+                if (dataSnapshot.getValue() != null) {
+                    //verilerimizi aldık
+                    taskList = new ArrayList<>();
+
+                    //tüm tasklar alındı.
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        UserTask usrtasks = postSnapshot.getValue(UserTask.class);
+                        taskList.add(usrtasks);
+                    }
+
+                    filterList();
+
+                } else {
+                }
+                loading.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(root.getContext(), R.string.controlInternet, Toast.LENGTH_LONG).show();
+                loading.dismiss();
+            }
+        });
+    }
+
+    private void filterList() {//listeyi seçili tarihe göre filtreleme
+        if (taskList != null) {
+            filteredList = new ArrayList<>();
+
+            for (UserTask list : taskList) {
+
+                //gün olarak seçilenlerin, seçili günde olup olmadığına bakılıp eklendi.
+                if (list.getIsActive() && list.getRepeatType() && (list.getRepeatInfo())
+                        .contains((cal.getDisplayName(cal.DAY_OF_WEEK, cal.SHORT, Locale.getDefault())).substring(0, 2))) {
+                    filteredList.add(list);
+                }
+
+                //tarih seçilenler kontrol edilerek eklendi.
+                if (list.getIsActive() && !list.getRepeatType()) {
+                    String[] dates = list.getRepeatInfo().split("/");
+
+                    if (cal.get(cal.DAY_OF_MONTH) == Integer.parseInt(dates[0]) &&
+                            (cal.get(cal.MONTH) + 1) == Integer.parseInt(dates[1]) &&
+                            cal.get(cal.YEAR) == Integer.parseInt(dates[2])) {
+                        filteredList.add(list);
+                    }
+                }
+            }
+
+            //liste başlatıldı.
+            startListView();
+        }
+    }
+
+    private void startListView(){
+
+        //list'in nasıl görüneceğinin adapterı
+        listAdapter = new TaskListAdapter(filteredList, getContext());
+
+        //bağlama işlemi yaptık
+        taskListView.setAdapter(listAdapter);
+
+        //edite tıklanma dinlemesi yapıldı
+        listenEditClicked();
+    }
+
+    private void listenEditClicked(){//edit için add sayfasına bilgiler ile birlikte yönlendirme yapıldı.
+        try {
+            listAdapter.geteditID().observe(getActivity(), new Observer<Integer>() {
+                @Override
+                public void onChanged(@Nullable Integer editId) {
+                    //do what you want when the varriable change.
+
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("selectedTask", filteredList.get(editId));
+
+                    NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                    navController.navigate(R.id.navigation_add, bundle);
+                }
+            });
+        }catch (Exception e){
+
+        }
     }
 
 }
