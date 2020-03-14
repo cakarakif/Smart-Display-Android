c10cfb459aa479ea3294791f4cadcfe02b189227
cakarakif <cakarakif@users.noreply.github.com>
2020-03-14 18:12:08 +0300

    notifications can be clickable

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ReminderAlarm/NotificationHelper.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ReminderAlarm/NotificationHelper.java
@@ -3,11 +3,15 @@ package com.example.smartdisplay.ReminderAlarm;
 import android.annotation.TargetApi;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.os.Build;
 
+import com.example.smartdisplay.MainActivity;
 import com.example.smartdisplay.R;
+import com.example.smartdisplay.ui.today.TodayFragment;
 
 import androidx.core.app.NotificationCompat;
 
@@ -41,10 +45,17 @@ public class NotificationHelper extends ContextWrapper {
     }
 
     public NotificationCompat.Builder getChannelNotification() {
+
+        //bildirime tıklandığında uygulama açıldı
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+
         return new NotificationCompat.Builder(getApplicationContext(), channelID)
                 .setContentTitle("Alarm!")
                 .setContentText("Your AlarmManager is working.")
                 .setSmallIcon(R.drawable.calendar)
+                .setContentIntent(contentIntent)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE);
     }
 }
