986171eace77e377cfea3c740e58cd8737343f3a
cakarakif <cakarakif@users.noreply.github.com>
2020-03-22 17:56:10 +0300

    Weather info without customized

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/SmartScreen.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/SmartScreen.java
@@ -3,22 +3,64 @@ package com.example.smartdisplay.SmartScreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartdisplay.R;
+import com.example.smartdisplay.SmartScreen.WeatherHelpers.CityPreference;
+import com.example.smartdisplay.SmartScreen.WeatherHelpers.RemoteFetch;
+
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SmartScreen extends AppCompatActivity {
+
+    //Weather değişkenleri
+    Typeface weatherFont;
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+    Handler handler;
+
+    public SmartScreen(){
+        handler = new Handler();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_screen);
 
+        define();
         setContentView();
 
+        updateWeatherData(new CityPreference(this).getCity());
+
+    }
+
+    private void define(){
+        cityField = findViewById(R.id.city_field);
+        updatedField = findViewById(R.id.updated_field);
+        detailsField = findViewById(R.id.details_field);
+        currentTemperatureField = findViewById(R.id.current_temperature_field);
+        weatherIcon = findViewById(R.id.weather_icon);
+
+        weatherIcon.setTypeface(weatherFont);
     }
 
     @SuppressLint("SourceLockedOrientationActivity")
@@ -29,4 +71,92 @@ public class SmartScreen extends AppCompatActivity {
         //ekranın yan olarak kullanılmasını sağlar
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    ////Weather Part
+    public void changeCity(String city){
+        updateWeatherData(city);
+    }
+
+    private void updateWeatherData(final String city){
+        new Thread(){
+            public void run(){
+                final JSONObject json = RemoteFetch.getJSON(getApplicationContext(), city);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(getApplicationContext(),
+                                    getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + " ℃");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt") * 1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+
+        } catch (Exception e) {
+            Log.i("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset){
+        int id = actualId / 100;
+        String icon = "";
+        if(actualId == 800){
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+                icon = getString(R.string.weather_sunny);
+            } else {
+                icon = getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch(id) {
+                case 2 : icon = getString(R.string.weather_thunder);
+                    break;
+                case 3 : icon = getString(R.string.weather_drizzle);
+                    break;
+                case 7 : icon = getString(R.string.weather_foggy);
+                    break;
+                case 8 : icon = getString(R.string.weather_cloudy);
+                    break;
+                case 6 : icon = getString(R.string.weather_snowy);
+                    break;
+                case 5 : icon = getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        weatherIcon.setText(icon);
+    }
+    ////Weather Part
+
 }
