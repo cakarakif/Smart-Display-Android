30d103de8997a20fdd7ea697d6b6a883679318a0
cakarakif <cakarakif@users.noreply.github.com>
2020-03-25 16:22:55 +0300

    Refresh date and weather info per hour

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/SmartScreen.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/SmartScreen.java
@@ -26,9 +26,13 @@ import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SmartScreen extends AppCompatActivity {
 
@@ -42,11 +46,6 @@ public class SmartScreen extends AppCompatActivity {
     //
     TextView dateInfo;
 
-    public SmartScreen() {
-        handler = new Handler();
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,11 +55,9 @@ public class SmartScreen extends AppCompatActivity {
         define();
         setContentView();
 
-        //konum alınıp weather tetiklendi
-        getLocationAndstartWeather();
+        //tarih ve hava durumu-her saat tetiklendi
+        scheduleRepeat();
 
-        //tarih atandı
-        setDate();
 
     }
 
@@ -75,6 +72,10 @@ public class SmartScreen extends AppCompatActivity {
         dateInfo = findViewById(R.id.dateInfo);
     }
 
+    public SmartScreen() {
+        handler = new Handler();
+    }
+    
     @SuppressLint("SourceLockedOrientationActivity")
     private void setContentView() {
         //Tam ekran kullanımı için
@@ -92,45 +93,77 @@ public class SmartScreen extends AppCompatActivity {
         //////////////////
     }
 
-    public void setDate() {
 
-        Date today = Calendar.getInstance().getTime();//getting date
-        SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM d, yyyy");//formating according to my need
-        String date = formatter.format(today);
-        dateInfo.setText(date);
+    /***********************************************/
+    //Weather ve Date her saat başlangıcında yenilendi
+    public void scheduleRepeat() {
+        //ilk açılışta değerler atandı
+        getLocationAndstartWeather();
+        setDate();
+
+        //her saat başlangıcında tekrar değerler yenilendi
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //konum alınıp weather tetiklendi
+                getLocationAndstartWeather();
+
+                //tarih atandı
+                setDate();
+            }
+        };
+        timer.schedule(task, millisToNextHour(), 1000 * 60 * 60);
     }
 
-    ////konum alınıp weather tetiklendi
-    public void getLocationAndstartWeather() {
+    private long millisToNextHour() {
+        LocalDateTime nextHour = LocalDateTime.now().plusHours(1).truncatedTo(ChronoUnit.HOURS);
+        return LocalDateTime.now().until(nextHour, ChronoUnit.MILLIS);
+    }
+
+    public void setDate() {
+        runOnUiThread(new Runnable() {//timer yapısı için gerekli rutin sadece
+
+            @Override
+            public void run() {
+
+                Date today = Calendar.getInstance().getTime();//getting date
+                SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM d, yyyy");//formating according to my need
+                String date = formatter.format(today);
+                dateInfo.setText(date);
+            }
+        });
+    }
+
+    public void getLocationAndstartWeather() {////konum alınıp weather tetiklendi
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            String longitude = location.getLongitude()+"";
-            String latitude = location.getLatitude()+"";
+            String longitude = location.getLongitude() + "";
+            String latitude = location.getLatitude() + "";
             //weather başlatıldı
-            updateWeatherData(latitude+"", longitude+"");
-        }else {
-            Toast.makeText(getApplicationContext(),getString(R.string.locationPermission),Toast.LENGTH_LONG).show();
+            updateWeatherData(latitude + "", longitude + "");
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.locationPermission), Toast.LENGTH_LONG).show();
         }
     }
-    //
 
     ////Weather Part
-    private void updateWeatherData(final String lat, final String lon){
-        new Thread(){
-            public void run(){
-                final JSONObject json = RemoteFetch.getJSON(getApplicationContext(), lat,lon);
-                if(json == null){
-                    handler.post(new Runnable(){
-                        public void run(){
+    private void updateWeatherData(final String lat, final String lon) {
+        new Thread() {
+            public void run() {
+                final JSONObject json = RemoteFetch.getJSON(getApplicationContext(), lat, lon);
+                if (json == null) {
+                    handler.post(new Runnable() {
+                        public void run() {
                             Toast.makeText(getApplicationContext(),
                                     getString(R.string.place_not_found),
                                     Toast.LENGTH_LONG).show();
                         }
                     });
                 } else {
-                    handler.post(new Runnable(){
-                        public void run(){
+                    handler.post(new Runnable() {
+                        public void run() {
                             renderWeather(json);
                         }
                     });
@@ -145,13 +178,13 @@ public class SmartScreen extends AppCompatActivity {
             JSONObject details = json.getJSONArray("weather").getJSONObject(0);
             JSONObject main = json.getJSONObject("main");
             detailsField.setText(
-                    details.getString("description").toUpperCase(Locale.US) );
+                    details.getString("description").toUpperCase(Locale.US));
 
             currentTemperatureField.setText(
-                    (int)((main.getDouble("temp")-273.15)) + " ℃");
+                    (int) ((main.getDouble("temp") - 273.15)) + " ℃");
 
             DateFormat df = DateFormat.getDateTimeInstance();
-            Date date=java.util.Calendar.getInstance().getTime();
+            Date date = java.util.Calendar.getInstance().getTime();
             setWeatherIcon(details.getInt("id"),
                     json.getJSONObject("sys").getLong("sunrise") * 1000,
                     json.getJSONObject("sys").getLong("sunset") * 1000);
@@ -161,36 +194,44 @@ public class SmartScreen extends AppCompatActivity {
         }
     }
 
-    private void setWeatherIcon(int actualId, long sunrise, long sunset){
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
         int id = actualId / 100;
-        int icon=0;
-        if(actualId == 800){
+        int icon = 0;
+        if (actualId == 800) {
             long currentTime = new Date().getTime();
-            if(currentTime>=sunrise && currentTime<sunset) {
+            if (currentTime >= sunrise && currentTime < sunset) {
                 icon = R.drawable.ic_weather_sunny;
             } else {
                 icon = R.drawable.ic_weather_clear_night;
             }
         } else {
-            switch(id) {
-                case 2 : icon = R.drawable.ic_weather_thunder;
+            switch (id) {
+                case 2:
+                    icon = R.drawable.ic_weather_thunder;
                     break;
-                case 3 : icon = R.drawable.ic_weather_drizzle;
+                case 3:
+                    icon = R.drawable.ic_weather_drizzle;
                     break;
-                case 7 : icon = R.drawable.ic_weather_foggy;
+                case 7:
+                    icon = R.drawable.ic_weather_foggy;
                     break;
-                case 8 : icon = R.drawable.ic_weather_cloudy;
+                case 8:
+                    icon = R.drawable.ic_weather_cloudy;
                     break;
-                case 6 : icon = R.drawable.ic_weather_snowy;
+                case 6:
+                    icon = R.drawable.ic_weather_snowy;
                     break;
-                case 5 : icon = R.drawable.ic_weather_rainy;
+                case 5:
+                    icon = R.drawable.ic_weather_rainy;
                     break;
-                default: icon= R.drawable.ic_thermometer;
+                default:
+                    icon = R.drawable.ic_thermometer;
                     break;
             }
         }
         weatherIcon.setBackground(getDrawable(icon));
     }
     ////Weather Part
+    /***********************************************/
 
 }
