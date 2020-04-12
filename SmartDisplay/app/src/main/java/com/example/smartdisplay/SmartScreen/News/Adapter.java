f45596d3c2d7a5698ec36e222e38e5a87cc8fb88
cakarakif <cakarakif@users.noreply.github.com>
2020-04-12 15:23:57 +0300

    fixed news layout view

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/News/Adapter.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/SmartScreen/News/Adapter.java
@@ -14,14 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.smartdisplay.R;
 import com.example.smartdisplay.SmartScreen.News.Model.Articles;
 import com.squareup.picasso.Picasso;
-
-import org.ocpsoft.prettytime.PrettyTime;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
@@ -54,7 +47,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         holder.tvTitle.setText(a.getTitle());
         holder.tvSource.setText(a.getSource().getName());
-        holder.tvDate.setText("\u2022"+dateTime(a.getPublishedAt()));
 
     }
 
@@ -64,7 +56,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tvTitle,tvSource,tvDate;
+        TextView tvTitle,tvSource;
         ImageView imageView;
         CardView cardView;
 
@@ -73,31 +65,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvSource = itemView.findViewById(R.id.tvSource);
-            tvDate = itemView.findViewById(R.id.tvDate);
             imageView = itemView.findViewById(R.id.image);
             cardView = itemView.findViewById(R.id.cardView);
 
         }
-    }
-
-
-    public String dateTime(String t){
-        PrettyTime prettyTime = new PrettyTime(new Locale(getCountry()));
-        String time = null;
-        try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:",Locale.ENGLISH);
-            Date date = simpleDateFormat.parse(t);
-            time = prettyTime.format(date);
-        }catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return time;
-
-    }
-
-    public String getCountry(){
-        Locale locale = Locale.getDefault();
-        String country = locale.getCountry();
-        return country.toLowerCase();
     }
 }
