d25a885ba053fe98d42ebb65116c20c91a2dadf0
cakarakif <cakarakif@users.noreply.github.com>
2020-05-07 13:59:39 +0300

    Controlled version to renew v2

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/alltasks/AllTasksFragment.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/alltasks/AllTasksFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,14 +16,11 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -32,17 +28,11 @@ import com.example.smartdisplay.Adapter.TaskListAdapter;
 import com.example.smartdisplay.DatabaseHelperClasses.DatabaseProcessing;
 import com.example.smartdisplay.DatabaseHelperClasses.UserTask;
 import com.example.smartdisplay.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class AllTasksFragment extends Fragment {
@@ -268,7 +258,14 @@ public class AllTasksFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 taskList=new ArrayList<>(tempSearchtaskList);;
-                taskList.removeIf(p -> !p.getTitle().toUpperCase().contains(charSequence.toString().toUpperCase()));
+                //taskList.removeIf(p -> !p.getTitle().toUpperCase().contains(charSequence.toString().toUpperCase()));
+
+                for (Iterator<UserTask> p = taskList.listIterator(); p.hasNext(); ) {
+                    UserTask a = p.next();
+                    if (!a.getTitle().toUpperCase().contains(charSequence.toString().toUpperCase())) {
+                        p.remove();
+                    }
+                }
 
                 //list'in nasıl görüneceğinin adapterı
                 listAdapter = new TaskListAdapter(taskList, getContext());
