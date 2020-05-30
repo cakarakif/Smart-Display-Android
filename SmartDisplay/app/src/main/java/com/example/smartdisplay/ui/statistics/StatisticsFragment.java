17f2aa643c0f49d45abb9d7c8c2cacead150a1ea
cakarakif <cakarakif@users.noreply.github.com>
2020-05-30 12:48:05 +0300

    statistics started

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
@@ -24,20 +24,7 @@ public class StatisticsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_statistics, container, false);
 
-        /*AddReminder ad=new AddReminder(root.getContext());
-        Calendar rightNow = Calendar.getInstance();*/
 
-        /*UserTask usrTask=new UserTask(true,"Kitap Vakti1","Yarım kalan kitabini okumalisin","1","15/03/2020",false,rightNow.get(Calendar.HOUR_OF_DAY)+":"+(rightNow.get(Calendar.MINUTE)+1),true,"--","99");
-        ad.setUserTask(usrTask);
-        ad.startAlarm();*/
-        //ad.cancelAlarm();
-
-
-        /*AddReminder ad2=new AddReminder(root.getContext());
-        UserTask usrTask2=new UserTask(true,"Read the book","Keep reading your book","1","Mon/Tue/Wed/Thu/Fri/Sat/Sun",true,(rightNow.get(Calendar.HOUR_OF_DAY)<10 ? ("0"+rightNow.get(Calendar.HOUR_OF_DAY)):rightNow.get(Calendar.HOUR_OF_DAY))+":"+
-                ((rightNow.get(Calendar.MINUTE)+1) < 10 ? ("0"+(rightNow.get(Calendar.MINUTE)+1)):(rightNow.get(Calendar.MINUTE)+1)),true,"--","3");
-        ad2.setUserTask(usrTask2);
-        ad2.startAlarm();*/
 
 
         return root;
