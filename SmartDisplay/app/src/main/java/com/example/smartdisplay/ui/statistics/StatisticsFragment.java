4ff03ad7a4fdfa9d14d8a26de0233e4251d964c4
cakarakif <cakarakif@users.noreply.github.com>
2020-05-31 15:03:34 +0300

    setted calendar view for statistic //deleted old structures

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
@@ -1,6 +1,8 @@
 package com.example.smartdisplay.ui.statistics;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +19,28 @@ import com.example.smartdisplay.R;
 import com.example.smartdisplay.ReminderAlarm.AddReminder;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class StatisticsFragment extends Fragment {
+    View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_statistics, container, false);
-
-
+        root = inflater.inflate(R.layout.fragment_statistics, container, false);
+        setLocaleEnglish();
+        root = inflater.inflate(R.layout.fragment_statistics, container, false);
 
 
         return root;
+    }
+
+    private void setLocaleEnglish(){
+        String languageToLoad  = "en_US"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        root.getContext().getResources().updateConfiguration(config,
+                getContext().getResources().getDisplayMetrics());
     }
 }
