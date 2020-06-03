f00c7f08a16dd81c19ba75e5a8ac98682725f19f
cakarakif <cakarakif@users.noreply.github.com>
2020-06-03 13:45:08 +0300

    Passed values into calendar dinamically

--- SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
+++ SmartDisplay/app/src/main/java/com/example/smartdisplay/ui/statistics/StatisticsFragment.java
@@ -1,7 +1,9 @@
 package com.example.smartdisplay.ui.statistics;
 
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -17,8 +20,12 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.smartdisplay.DatabaseHelperClasses.UserTask;
 import com.example.smartdisplay.R;
 import com.example.smartdisplay.ReminderAlarm.AddReminder;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -38,7 +45,6 @@ public class StatisticsFragment extends Fragment {
         define();
         setDatesIntoCalendar();
 
-        Log.i("kntrlAkif",taskList.get(0).getTitle()+""+taskList.get(1).getTitle());
 
         return root;
     }
@@ -57,15 +63,53 @@ public class StatisticsFragment extends Fragment {
     }
 
     private void setDatesIntoCalendar(){
-        calendar.setSelectionMode (MaterialCalendarView.SELECTION_MODE_MULTIPLE);
+        calendar.setSelectionMode (MaterialCalendarView.SELECTION_MODE_SINGLE);
+        for (UserTask task : taskList) {
+            //gün olarak seçilenlerin, seçili günde olup olmadığına bakılıp eklendi.
+            if (task.getIsActive() && task.getRepeatType()) {
 
-        Calendar calendar0 = Calendar.getInstance();
-        calendar0.set(2020, 06, 11);
+                calendar.addDecorator(new DayViewDecorator() {
+                    @Override
+                    public boolean shouldDecorate(CalendarDay day) {
+                        return (task.getRepeatInfo())
+                                .contains((day.getCalendar().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault())).substring(0, 3));
+                    }
 
-        Calendar calendar1 = Calendar.getInstance();
-        calendar1.set(2020, 06, 19);
+                    @Override
+                    public void decorate(DayViewFacade view) {
+                        // add foreground span
+                        view.addSpan(new ForegroundColorSpan(
+                                ContextCompat.getColor(getContext(), R.color.yellow)));
+                    }
+                });
 
-        calendar.setDateSelected(calendar0, true);
-        calendar.setDateSelected(calendar1, true);
+            }
+
+            //tarih seçilenler kontrol edilerek eklendi.
+            else if (task.getIsActive() && !task.getRepeatType()) {
+
+                calendar.addDecorator(new DayViewDecorator() {
+                    @Override
+                    public boolean shouldDecorate(CalendarDay day) {
+                        Calendar cal1 = day.getCalendar();
+                        Calendar cal2 = Calendar.getInstance();
+                        cal2.set(task.getYear(), task.getMonth()-1, task.getDay());
+
+                        return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA)
+                                && cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                                && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR));
+                    }
+
+                    @Override
+                    public void decorate(DayViewFacade view) {
+                        // add foreground span
+                        view.addSpan(new ForegroundColorSpan(
+                                ContextCompat.getColor(getContext(), R.color.yellow)));
+                    }
+                });
+            }
+
+
+        }
     }
 }
